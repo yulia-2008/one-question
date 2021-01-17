acfb029dc07aa03e95ea547b29c1c0b9768f087a
yulia <juliana.ny2008@gmail.com>
2021-01-17 15:49:48 -0500

    practicing React Native

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("Yulia")
+  const clickHandler = () => {
+    setName("Olga");
+  }
   return (
     <View style={styles.container}>
-      <Text>TEST appppppp</Text>
+      <Text>My name is {name}</Text>
+      <View style={styles.test}>
+        <Button title="update name"  onPress = {clickHandler}/>
+      </View>
+      <Text> Enter name:</Text>
+      <TextInput 
+          style={styles.input}
+          placehodle= "Enter your name"
+          onChangeText={(val) => setName(val)}
+          keyboardType='numeric'/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +31,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  test: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'red',
+    margin: 10,
+    width: 200,
+  }
 });
