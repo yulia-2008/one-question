3b067ed4b5b43aff53ace93efa5da8592118d78c
yulia <juliana.ny2008@gmail.com>
2021-01-25 11:07:23 -0500

    playning with styles

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, ScrollView } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons'; 
 
 export default function App() {
   const [tab, addTab] = useState(["first tab"])
@@ -29,21 +30,21 @@ export default function App() {
       
         <ScrollView horizontal={true}>
         {tab.map(tab=> {
-            return <View>
+            return <View style={styles.box}>
             
             <TextInput style={styles.input} 
                               onChangeText={text=>setTabName(text)}
                               placeholder='enter tab name'
                               onPress={clickHandler}
                               multiline /> 
-            <AntDesign name="delete" size={24} color="black" />
+            <AntDesign  name="delete" size={25} color="black" />
             </View>
         })}
         </ScrollView>
        
 
-        <TouchableOpacity onPress={pressHandler}>
-          <Text style={styles.button}>Add Tab</Text>
+        <TouchableOpacity style={styles.button} onPress={pressHandler}>
+            <Ionicons name="add" size={50} color="black" />
         </TouchableOpacity>
 
       </View>
@@ -56,19 +57,33 @@ const styles = StyleSheet.create({
     // flex: 1,
     marginTop: 30,
     flexDirection: 'row',
-    backgroundColor: 'gray',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 0, 
   },
   input: {
-    marginTop: 5,
+    margin: 0,
     padding: 0,
-    backgroundColor: 'pink', 
+    backgroundColor: 'white', 
+    // borderWidth: 1,
+    textAlign: 'center',
+    // borderRadius: 5 ,
+  },
+  box: {
+    flex: 1,
+    backgroundColor: '#D1D1D3', 
     borderWidth: 1,
-    textAlign: 'center'
+    borderRadius: 7,
+    flexDirection: 'row', 
+    padding: 5,
+    margin: 5,
   },
   button: {
-    backgroundColor: 'pink', 
+    backgroundColor: 'grey', 
+    margin: 0, 
+    padding: 0, 
+    borderWidth: 1,
   }
   
 });
