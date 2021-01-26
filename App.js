3e7a3938c1d06adda7b0ac27696dee7fd73c5f2c
yulia <juliana.ny2008@gmail.com>
2021-01-26 14:40:04 -0500

    deleting memos

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, ScrollView, Alert } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons'; 
 
@@ -16,23 +16,35 @@ export default function App() {
   //   setTabName(event.target.title);  
   // }
 
- const pressHandler = () => {
-   let findKey = memos.length
+const addHandler = () => {
+   let createKey = Math.floor(Math.random() * 100)
    updateMemos(prev => { return [
-      {name: "added memo", key: findKey+1}, ...prev
+      {title: "new memo", key: createKey}, ...prev
     ]}) 
- }
+}
 
- const deleteHandler = key => {
-   console.log(key)
-    let filteredMemos = memos.filter(memo => memo.key != key)
-    updateMemos (prev => {return filteredMemos
-    })
- }
+const deleteHandler = memoForDeletion => {
+  Alert.alert(
+     "Delete?", 
+      `Delete "${memoForDeletion.title}" with all its content?`,
+      [
+        { text: "OK", 
+          onPress: ()=> {
+            let filtered = memos.filter(memo => memo.key != memoForDeletion.key)
+            updateMemos (prev => {return filtered})
+        }},
 
-//  const clickHandler =()=>{
-   
-//  }
+        { text: 'Cancel',  
+          onPress: () => console.log('Cancel Pressed')    
+        }
+      ] 
+  )        
+}
+
+const editHandler = memoforEditing => {
+  // foundMemo = memos.find(memo => memo.key === memoforEditing.key) 
+  // foundMemo.title
+}
 
 
   
@@ -43,23 +55,27 @@ export default function App() {
           <ScrollView horizontal={true}>
             {memos.map(memo=> {
             return <View style={styles.inputBox} key={memo.key}>
-            <Text> {memo.name}</Text>
+            <Text> key: {memo.key}</Text>
+            <Text> title: {memo.title}</Text>
             
             {/* <TextInput style={styles.input} 
                               onSubmitEditing={text=>setMemoName(text)}
                               placeholder=' memo name '
                               
                               />  */}
-            <TouchableOpacity  onPress={memo => {deleteHandler(memo.key)}}>
-            <AntDesign name="delete" size={25} color="black" />
+            <TouchableOpacity  onPress={arg => {deleteHandler(memo)}}>
+              <AntDesign name="delete" size={25} color="black" />
             </TouchableOpacity>
 
-            <AntDesign name="edit" size={24} color="black" />
+            <TouchableOpacity  onPress={arg => {editHandler(memo)}}>
+              <AntDesign name="edit" size={24} color="black" />
+            </TouchableOpacity>
+
             </View>
             })}
           </ScrollView>
 
-          <TouchableOpacity style={styles.button} onPress={pressHandler}>
+          <TouchableOpacity style={styles.button} onPress={addHandler}>
             <Ionicons name="add" size={50} color="black" />
           </TouchableOpacity>
 
