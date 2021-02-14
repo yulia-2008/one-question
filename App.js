4408c9c9bbb18cf1745e9e7aeaad3ff476a1ac49
yulia <juliana.ny2008@gmail.com>
2021-02-14 15:44:23 -0500

    testing asyncStorage

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ export default function App() {
     updateMemos(prev => { return [ newMemo, ...prev
       ]}) 
     setCurrentMemo(newMemo) 
+    
     // testing asyncStorage !!!
     
         AsyncStorage.setItem("key", "age")
